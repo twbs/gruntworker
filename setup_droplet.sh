a1c6c9286a839127ec7456f6e336d862e643311b
Chris Rebert <code@chrisrebert.com>
2015-11-02 00:02:07 -0800

    finish unDockerized draft

--- setup_droplet.sh
+++ setup_droplet.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-# Step 0.0: Put SSH keys in ./ssh
-# Step 0.1: Checkout git repo to /opt/gruntworker/git-repo
-
 set -e -x
 
 # set to Pacific Time (for @cvrebert)
@@ -30,5 +27,33 @@ aptitude install nodejs
 aptitude install nodejs-legacy # out of compatibility paranoia, though I philosophically agree with Debian here
 
 # setup gruntworker itself
-cp ./gruntworker.crontab /etc/cron.d/gruntworker
+apt-get -y --no-install-recommends install openssh-client git python3 python3-dev # other dependencies
+npm install -g grunt-cli # dependency
+git clone git@github.com:twbs/gruntworker.git ~/gruntworker
+mkdir -p /usr/local/bin
+cp ~/gruntworker/gruntworker.sh /usr/local/bin/
+cp ~/gruntworker/gruntworker.py /usr/local/bin/
+chmod u=rwx,go=rx /usr/local/bin/gruntworker.*
+useradd gruntworker
+
+# setup SSH keys
+su gruntworker # USER: gruntworker
+mkdir ~/.ssh
+ssh-keyscan -t rsa github.com > ~gruntworker/.ssh/known_hosts
+# MANUAL STEP: Put SSH keys in /home/gruntworker/.ssh
+chmod -R go-rwx ~/.ssh
+# setup git & repo
+git clone git@github.com:twbs/bootstrap.git ~/git-repo
+cd ~/git-repo
+git remote set-url origin https://github.com/twbs/bootstrap.git
+git remote set-url --push origin git@github.com:twbs/bootstrap.git
+git config core.fileMode false
+git config user.name "Bootstrap's Grunt bot"
+git config user.email 'twbs-grunt@users.noreply.github.com'
+# setup Bootstrap
+npm install
+
+# setup cron
+exit # USER: root
+cp ~/gruntworker/gruntworker.crontab /etc/cron.d/gruntworker
 restart cron # until upstart goes away
